5d5a302d01ef2028c44f753c88c455fa2e0da9fd
jpumaNHA <j.puma@nhassociates.com>
2025-03-19 20:28:51 -0400

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -41,11 +41,11 @@ class _UiSettings(BaseSettings):
         env_ignore_empty=True
     )
 
-    title: str = "Contoso"
+    title: str = "Neil Hoosier & Associates, Inc."
     logo: Optional[str] = None
     chat_logo: Optional[str] = None
-    chat_title: str = "Start chatting"
-    chat_description: str = "This chatbot is configured to answer your questions"
+    chat_title: str = "What can I help with?"
+    chat_description: str = "Ask anything"
     favicon: str = "/favicon.ico"
     show_share_button: bool = True
     show_chat_history_button: bool = True
